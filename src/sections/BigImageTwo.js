dc4562110518aa0b513a29a72327779cc9fadbd2
Akilesh <akileshakileshs1234@gmail.com>
2025-01-28 15:38:34 +0530

    Finished completely responsive BigImageTwo and ToyotaRange section

--- src/sections/BigImageTwo.js
+++ src/sections/BigImageTwo.js
@@ -1,5 +1,11 @@
+import image from "../images/BigImageTwo/virtual-showroom-1920x807.jpg";
+
+import "./BigImageTwo.css"
+
 export const BigImageTwo = () => {
   return (
-    <div>BigImageTwo</div>
+    <div className="BigImageTwo">
+      <img src={image} alt="Big Two" />
+    </div>
   )
 }
